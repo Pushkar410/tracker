729239e847fae3f03e1a9cf24a221758d01c0a9b
Your Name <killerram01@gmail.com>
2026-03-22 20:06:07 +0530

    feat: added os tracker and navigation buttons

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // Use lc8w.html as the entry point
+  // Use index.html as the entry point
   build: {
     rollupOptions: {
-      input: './lc8w.html'
+      input: './index.html'
     }
   }
 })
